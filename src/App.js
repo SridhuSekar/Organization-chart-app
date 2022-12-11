a122ff9da8dae022e5dc9a477649c36ea42ba396
SridhuSekar <92387510+SridhuSekar@users.noreply.github.com>
2022-12-11 13:24:01 +0530

    Update App.js
    Footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import TreeChart from './components/TreeChart';
+import Footer from './components/Fotter';
 
 function App() {
   return (
     <div className="App">
       <h1>Organization Chart</h1>
       <TreeChart />
+      <Footer />
     </div>
   );
 }
