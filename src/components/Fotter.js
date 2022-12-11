56239190bb31b87c7e6d073b67391874d37a8ff8
SridhuSekar <92387510+SridhuSekar@users.noreply.github.com>
2022-12-11 13:23:07 +0530

    Add files via upload

---
+++ src/components/Fotter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TreeChart.css'
+
+function Footer() {
+  return (
+    <div className='footer'>Copyrights 2022 &copy; <strong>Sridhu</strong></div>
+  )
+}
+
+export default Footer
